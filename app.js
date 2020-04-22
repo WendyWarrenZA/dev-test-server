6b68291b1e35ef0c5745c7b150b67323c9fa69a2
Wendy Warren <wwarren@dvt.co.za>
2020-04-22 14:22:20 +0200

    Added code to calculate commit counts

--- app.js
+++ app.js
@@ -14,26 +14,71 @@ app.get("/", (req, res) => {
 //Get /commits?owner=test&repo=one&since=YYYY-MM-DDTHH:MM:SSZ&until=YYYY-MM-DDTHH:MM:SSZ
 //since is commits after this date, unit is commits before this date
 app.get('/commits', (req, res) => {
-	if (req.query.owner == null || req.query.repo == null || req.query.since == null || req.query.until == null){
-		res.send('No parameters, please include owner, repo, since and until');
+	if (req.query.owner == null || req.query.repo == null || req.query.since == null || req.query.until == null || req.query.interval == null){
+		res.send('No parameters, please include owner, repo, since, until and interval');
 	}
 	else{
 		const owner = req.query.owner;
 		const repo = req.query.repo;
 		const since = req.query.since;
-		const until = req.query.until;
+		const until = req.query.until;		
+		let pageIndex = 1;
+		let commits = [];
 		
-		let url = 'https://api.github.com/repos/'+owner+'/'+repo+'/commits?per_page=1&since='+since+'&until='+until;
-		(async () => {
+		//Get the first page and determine if more pages need to be obtained from the github api
+		let url = 'https://api.github.com/repos/'+owner+'/'+repo+'/commits?per_page=100&since='+since+'&until='+until+'&page=1';
+		(async () => {						
 			try {
-				const response = await got(url);							
-				//res.send(response.body);
-				res.send('{"interval": "week","commitCounts": {"trueadm" : 1,"bvaughn" : 4, "sjones" : 0, "fbower" : 5}}');
+				let response = await got(url);	
+				if (response != null){
+					commits = JSON.parse(response.body);						
+					if (response.headers.link != null){
+						let newUrl = response.headers.link.split(',')[1].split(';')[0].split('<')[1].split('>')[0];							
+						pageIndex = newUrl.split('&page=')[1];						
+					}
+				}	
 			} catch (error) {			
-				//=> 'Internal server error ...'
+				//=> 'Internal server error ...'	
+				console.log(error);				
+			}
+			//get more pages if necessary			
+			if (pageIndex > 1) {	
+				for (let i = 2; i <= pageIndex; i++){
+					try {			
+						url = 'https://api.github.com/repos/'+owner+'/'+repo+'/commits?per_page=100&since='+since+'&until='+until+'&page='+i;										
+						let response = await got(url);	
+						if (response != null){	
+							commits.push(JSON.parse(response.body));							
+						}
+					} catch (error) {			
+						//=> 'Internal server error ...'	
+						console.log(error);				
+					}			
+				}
+			}		
+		})().then(() => {
+			//process the reponse to get the commit counts per user
+			try{
+				var commitCounts = {}; 				
+				for (let i = 0; i < commits.length; i++)
+				{
+					if (commits[i].author != null){									
+						let commit = commits[i].author.login;        
+						if (!commitCounts[commit]){
+							commitCounts[commit] = 1;                        
+						} else {
+							commitCounts[commit] += 1;
+						}
+					}					
+				}
+				
+				res.send({"interval": req.query.interval,"commitCounts": commitCounts});
+			} catch (error) {			
+				//=> 'Internal server error ...'	
+				console.log(error);						
 				res.send('Error: ' + error);
 			}
-		})();
+		});		
 	}
 });
 
